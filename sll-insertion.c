9b2da6d9996fc75ea647e83e4537d44f23bf657c
S-Anuj-Srijan <anuj.srijan.s@gmail.com>
2025-10-20 16:09:30 +0530

    all insert function done

--- sll-insertion.c
+++ sll-insertion.c
@@ -23,6 +23,15 @@ void InsertAtBegining(int data , node **head){
     }
 
 }
+void InsertAtEnd(int data , node ** head ){
+    node *temp = *head; 
+    node * newnode = CreatNode(data);
+    while(temp->link!=NULL){
+        temp=temp->link; 
+    }
+    temp->link=newnode;
+
+}
 void PrintNodes(node * head){
     node *temp = head;
     while(temp!=NULL){
@@ -32,12 +41,29 @@ void PrintNodes(node * head){
 
 }
 
+void InsertAtPos(int data , int pos , node **head){
+    node *temp = *head;
+    node *newnode=CreatNode(data);
+    for(int i = 0 ; i<=pos && temp!= NULL ;i++){
+        temp=temp->link;
+    }
+    if (temp==NULL){
+        free(newnode);
+        printf("wrong pos val");
+    }
+    else {
+        newnode->link=temp->link;
+        temp->link=newnode;
+    }
+}
 int main(){
     node *head=NULL; 
     InsertAtBegining(1,&head);
     InsertAtBegining(2,&head);
     InsertAtBegining(3,&head);
     InsertAtBegining(4,&head);
+    InsertAtEnd(5,&head);
+    InsertAtPos(32,0,&head);
     PrintNodes(head);
 
 }
